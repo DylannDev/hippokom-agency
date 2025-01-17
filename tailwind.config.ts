e2fb773ba266d0ea2ed277909f4d98b81807ea3e
dylann97 <d.xavero@hotmail.com>
2025-01-17 15:09:29 +0100

    🚧 Installed react-icon and add new colors in tailwind config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,10 +10,24 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        blue: {
+          background: "#f3f8ff",
+          light: "#B7E9FD",
+          medium: "#52B8EC",
+          DEFAULT: "#436AF6",
+          hover: "#3d60e0",
+          dark: "#15214B",
+        },
+        yellow: {
+          hover: "#fae55b",
+          DEFAULT: "#F9DE32",
+        },
+      },
+      borderRadius: {
+        "4xl": "32px",
+        "5xl": "40px",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
